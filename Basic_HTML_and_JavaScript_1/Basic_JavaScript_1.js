76f89e12d38ddc1ac22ed041b8b584d874561f01
ghvp <viviennepadilla14@gmail.com>
2022-10-06 23:33:02 -0700

    Added new folder and files

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<script>
+	alert("hello world");
+	</script>
+	<script>
+			function My_First_Function() {
+			var String = "Kiss me, I'm Irish!";
+			var result = String.fontcolor("green")
+			document.getElementById("Irish").innerHTML =
+			result;
+	}
+		</script>
+</html>
